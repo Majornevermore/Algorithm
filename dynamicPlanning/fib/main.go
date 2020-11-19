85702777d670d2e50c4f866971ebf158abffa1fc
Xin Lee <785304398@qq.com>
2020-11-19 17:32:40 +0800

    动态规划爬楼梯

---
+++ dynamicPlanning/fib/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var memo []int64
+
+func fib(n int) int64 {
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+	if memo[n] == -1 {
+		memo[n] = fib(n-1) + fib(n-2)
+	}
+	return memo[n]
+}
+
+func main() {
+	time1 := time.Now()
+	n := 200
+	memo = make([]int64, n+1)
+	for i := 0; i <= n; i++ {
+		memo[i] = -1
+	}
+
+	res := fib(n)
+	time2 := time.Now().Sub(time1)
+	fmt.Println(res, "time", time2)
+}
